a995f8d7294fa6257d0b5dbf3a843d45c35ad736
W. Brandon Comfort <wbgc728@gmail.com>
2022-05-23 22:27:56 -0600

    merged routes into server.js

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const path = require('path');
 const { clog } = require('./middleware/clog');
-const api = require('./routes/index.js');
+const { v4: uuidv4 } = require('uuid');
+// const api = require('./routes/index.js');
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require('./helpers/fsUtils');
 
 const PORT = process.env.PORT || 3001;
 
@@ -13,10 +19,9 @@ app.use(clog);
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+// app.use('/api', api);
 
 app.use(express.static('public'));
-
 
 // GET Route for notes page
 //return notes.html
@@ -24,12 +29,57 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// GET Route for retrieving all the notes
+// /api/note
+app.get('/api/notes', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+
 // GET Wildcard Route for homepage
 // Return index.html
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+// POST Route for a new Note
+// /api/notes
+app.post('/api/notes', (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuidv4(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note added successfully 🚀`);
+  } else {
+    res.error('Error adding note');
+  }
+});
+
+
+// DELETE Route for a specific note
+app.delete('/api/:note_id', (req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all notes except the one with the ID provided in the URL
+      const result = json.filter((note) => note.note_id !== noteId);
+
+      // Save that array to the filesystem
+      writeToFile('./db/db.json', result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${noteId} has been deleted 🗑️`);
+    });
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
